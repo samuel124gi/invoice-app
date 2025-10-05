089d90a996e3a8ebbaea322c8dcca210bc200343
samuel124gi <samjayi124@gmail.com>
2025-10-05 19:22:28 +0100

    invoice

--- src/components/Status/index.tsx
+++ src/components/Status/index.tsx
@@ -1,14 +1,13 @@
 import { StyledStatus, StatusIndicator } from "./styles";
 import { StatusVariant } from "../../types/component";
 
-interface ButtonProps {
+interface StatusProps {
   type: StatusVariant;
   label?: string;
   children?: React.JSX.Element[] | React.JSX.Element;
-  onClick?: () => void;
 }
 
-export const Status = ({ type, label, children }: ButtonProps) => {
+export const Status = ({ type, label, children }: StatusProps) => {
   return (
     <StyledStatus type={type}>
       <StatusIndicator type={type} />
